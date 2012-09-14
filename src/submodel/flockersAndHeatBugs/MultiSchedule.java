d30ced3221347632c675dd2058129180acf4d0c8
joey.f.harrison <joey.f.harrison@gmail.com>
2012-09-14 04:18:57 +0000

    Added initial implementations of getTime(), step(..), and reset().

--- src/submodel/flockersAndHeatBugs/MultiSchedule.java
+++ src/submodel/flockersAndHeatBugs/MultiSchedule.java
@@ -17,9 +17,40 @@ public class MultiSchedule extends Schedule
 		super();
 		this.schedules = schedules;
 	}
+	
+	@Override
+	public double getTime() {
+		double minTime = Schedule.AFTER_SIMULATION;
+		for (int i = 0; i < schedules.length; i++)
+			minTime = Math.min(minTime, schedules[i].getTime());
+		
+		return minTime;
+	}
 
 	@Override
 	public synchronized boolean step(SimState state) {
-		return super.step(state);
+		double minTime = Schedule.AFTER_SIMULATION;
+		Schedule minSchedule = null;
+		for (int i = 0; i < schedules.length; i++)
+			if (schedules[i].getTime() < minTime) {
+				minTime = schedules[i].getTime();
+				minSchedule = schedules[i];
+			}
+		
+		if (minSchedule != null)
+			minSchedule.step(state);	//TODO pass it the right SimState
+		
+		return (minTime < Schedule.AFTER_SIMULATION);
 	}
+
+	/**
+	 * Reset this schedule and all the constituent schedules.
+	 */
+	@Override
+	public void reset() {
+		super.reset();
+		for (int i = 0; i < schedules.length; i++)
+			schedules[i].reset();
+	}
+	
 }
