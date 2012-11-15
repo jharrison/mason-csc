c2ff5851d9a9d7fd18155f97cf9d55121be49ef1
fyedernoggersnodden <fyedernoggersnodden@gmail.com>
2012-11-15 19:27:52 +0000

    First commit of Joey and my data logging system for use with MASON models.

---
+++ DataWatcher/src/datawatcher/ScreenDataWriter.java
@@ -0,0 +1,24 @@
+package datawatcher;
+
+
+
+/**
+ *
+ * @author Eric 'Siggy' Scott
+ * @author Joey Harrison
+ */
+public class ScreenDataWriter implements DataListener
+{
+    public ScreenDataWriter(DataWatcher source)
+    {
+        // XXX Move this out of constructor?
+        System.out.println(source.getCSVHeader());
+        source.addListener(this);
+    }
+    
+    @Override
+    public void dataUpdated(DataWatcher source)
+    {
+        System.out.println(source.dataToCSV());
+    }
+}
